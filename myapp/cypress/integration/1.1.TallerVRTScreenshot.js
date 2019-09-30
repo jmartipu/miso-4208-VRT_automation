b78b1a112ab6d2ff6ca2663a3d70f21a2cca9245
Jorge Martín <jormartin77@gmail.com>
2019-09-29 20:41:57 -0500

    times

--- myapp/cypress/integration/1.1.TallerVRTScreenshot.js
+++ myapp/cypress/integration/1.1.TallerVRTScreenshot.js
@@ -1,11 +1,14 @@
 describe('VRT Screenshot', function() {
     it('Visit and take a screenshot', function() {
         cy.visit('https://jmartipu.github.io/palette.html');
+        cy.wait(3000)
         let now = new Date();
         cy.get('button[id="generar"]').click();
+        cy.wait(3000)
         let nombre1 = "Antes";
         cy.screenshot(nombre1);
       	cy.get('button[id="generar"]').click();
+      	cy.wait(3000)
       	let nombre2 = "Despues";
 		cy.screenshot(nombre2);
 		cy.wait(1000)
